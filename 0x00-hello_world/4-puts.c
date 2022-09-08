af0d0bd9b26d48a61487ebfab29af80f63e30fa0
Tjaysho007 <tunjishobaloju@gmail.com>
2022-09-08 14:04:25 -0700

    output using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main: print the string in the pust fuction
+ * Description: using main funtion
+ * this program prints 'programming is like building a multilingual puzzle
+ * return:0
+ */
+int main () 
+
+{   // string initalization 
+	char Mystr[ ] ="Programming is like building a multilingual puzzle";
+
+	puts(Mystr);  
+
+
+	return 0;
+}
