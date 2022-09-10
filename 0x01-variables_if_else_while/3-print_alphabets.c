82216a06250caed6d87215a1c092201fe3fae1f8
Tjaysho007 <tunjishobaloju@gmail.com>
2022-09-10 14:48:06 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (sucess)
+*/
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z' ; low++)
+		putchar(low);
+	for (low = 'A'; low <= 'z' ; low++)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
