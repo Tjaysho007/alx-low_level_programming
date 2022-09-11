217786358319eed642b7611a2d9e733fce17a8b3
Tjaysho007 <tunjishobaloju@gmail.com>
2022-09-11 08:55:04 -0700

    output with write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 1 (success)
+*/
+int main(void)
+{
+	write(" and the piece of art is useful\" - Dora Korpar, 2015-10-9\n", 59);
+	return (1);
+
+}
