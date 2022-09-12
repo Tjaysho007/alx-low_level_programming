4c94305532dec90fd50df6bba84a44684da8c149
Tjaysho007 <tunjishobaloju@gmail.com>
2022-09-12 07:47:36 -0700

    print alphabet lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main-Entry point
+* Descrotion: the code prints a - z
+* Return: Always (0) (success)
+*/
+
+int main(void)
+{
+	char low;
+
+	 for (low = 'a'; low <= 'z'; low++)
+		 
+		 putchar(low);
+
+	 	putchar('\n');
+		
+		return (0);
+}
