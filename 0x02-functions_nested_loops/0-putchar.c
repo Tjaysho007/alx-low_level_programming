17545a6864e25c5fd5d32bf7c804a63eecf1cf41
Tjaysho007 <tunjishobaloju@gmail.com>
2022-09-13 06:16:51 -0700

    output _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: a program that dispalys _putchar
+*Return Always 0 success
+*/
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	return (0);
+}
