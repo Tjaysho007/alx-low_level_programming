61e3e88315314bae6924dbeec2153afa259f618e
Tjaysho007 <tunjishobaloju@gmail.com>
2022-09-14 15:24:41 -0700

    print alpahbet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - print alphabet x10
+*
+*Return 0 alway
+*/
+
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0  ; i <= 9 ; i++)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar (c);
+
+		}
+		_putchar ('\n');
+	}
+
+}
