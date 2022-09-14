5834920cd5c7cd5d3e0caf67c35c6a4c99f568ab
Tjaysho007 <tunjishobaloju@gmail.com>
2022-09-14 16:08:45 -0700

    function to print lowercase letter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*main - checks for lower case letter
+*
+*Return 0 always
+*/
+
+int main(void)
+{
+	if (c >= 'a' && c <= 'z')
+
+{
+	return (1);
+}
+	else
+
+	return (0);
+
+}
