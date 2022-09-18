acee3084835dd955469488793ca5584a31acea8b
Tjaysho007 <tunjishobaloju@gmail.com>
2022-09-18 04:50:03 -0700

    print hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,13 +14,13 @@ int main(void)
 	char low;
 
 	for (d = 0 ; d <= 9 ; d++)
-	{
+
 		putchar(d);
-	
+
 	for (low = 'a' ; low <= 'f' ; low++)
-	
+
 		putchar(low);
-	}
+
 	putchar('\n');
 	return (0);
 }
