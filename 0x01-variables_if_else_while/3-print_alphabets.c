c97d42270216d6c3e876caf2295282415ce7de73
Tjaysho007 <tunjishobaloju@gmail.com>
2022-09-17 18:08:47 -0700

    lower and upper case alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+*main - print upper and lowwer case alphabet
+*
+*Description: using the main function
+*
+*Return: 0
+*/
+
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a' ; letter < 'z' ; letter++)
+	{
+		putchar(letter);
+
+	}
+	for (letter = 'A' ; letter < 'Z' ; letter++)
+	{
+		putchar(letter);
+
+	}
+	putchar('\n');
+
+	return (0);
+}
