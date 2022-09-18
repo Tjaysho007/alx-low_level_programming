03860f912d8ca4c368f5c4c10873d92e0144083b
Tjaysho007 <tunjishobaloju@gmail.com>
2022-09-17 17:42:13 -0700

    print using putchar

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,11 @@
 int main(void)
 {
 	char low;
-	char high;
 
 	for (low = 'a'; low <= 'z' ; low++)
+
 		putchar(low);
 
-	for (high = 'A'; high <= 'z' ; high++)
-		putchar(high);
 	putchar('\n');
 
 	return (0);
