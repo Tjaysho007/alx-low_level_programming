c2f6cf41e029291b3e38dfc5aafd52a7d2d4e431
Tjaysho007 <tunjishobaloju@gmail.com>
2022-09-18 03:25:58 -0700

    print base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ *main - prknt base 10 digits starting from 0
+ *
+*Description: this ptogram prints base 10 numbers
+*
+*Return 0
+*/
+int main(void)
+{
+	int j;
+
+	for (j = 0 ; j < 10 ; j++)
+	{
+		printf("%d", j);
+	}
+	printf("\n");
+
+	return (0);
+}
+
