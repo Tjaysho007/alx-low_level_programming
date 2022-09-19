d83dc7f07a39369f8b9178801e1ab65a1e43e24b
Tjaysho007 <tunjishobaloju@gmail.com>
2022-09-19 12:38:39 -0700

    string lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ *_strlen - returns the lenght of a string
+ *
+ *@s:string
+ *
+ *Return: returns lenght as an integer
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+
+}
