f8343e778aaeec7881335cbdae9c65a3bb2f5ec2
Tjaysho007 <tunjishobaloju@gmail.com>
2022-09-30 18:13:36 -0700

    adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - A function that adds positive  numbers
+ * @argc: parameter
+ * @argv:an array of command line
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int res = 0, num, i, j, k;
+
+	for  (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				printf("%s\n", "Error");
+
+				return (1);
+			}
+
+
+		}
+
+
+	}
+		if (argv[i][j] > '9' || argv[i][j] < '0')
+		{
+
+			num = atoi(argv[k]);
+			res += num;
+		}
+		printf("%d\n", res);
+		return (0);
+
+
+}
