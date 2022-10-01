a6c313a23db24688e8764c16f1bc68faa18a235a
Tjaysho007 <tunjishobaloju@gmail.com>
2022-09-30 17:31:06 -0700

    prints all arguments it recieves

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
 
 /**
- * main - A function that prints the number of arguments
+ * main - A function that prints all arguments it receives
  * @argc: parameter
  * @argv:an array of command line
  * Return: 0 success
  */
-int main(int argc, char *argv[] __attribute__ ((unused)))
+int main(int argc, char *argv[])
 {
-       	printf("%d\n", argc - 1);
-       	return (0);
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
 }
