f81da047861e0e4b0fe14b7f7fda744ac3eed82b
Tjaysho007 <tunjishobaloju@gmail.com>
2022-10-10 15:28:00 -0700

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,10 +16,10 @@ struct dog
 	char *owner;
 
 };
-typedef struct dog dog_d;
+typedef struct dog dog_t;
 void init_dog(struct dog *t, char *name, float age, char *owner);
-void print_dog(struct dog *t);
-void free_dog(dog_d *t);
+void print_dog(struct dog *d);
+void free_dog(dog_d *d);
 
 #endif /*DOG_H*/
 
