93465d3511bf7719be8f829ccca03a7576050b15
Tjaysho007 <tunjishobaloju@gmail.com>
2022-10-21 16:00:01 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -22,7 +22,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (str == NULL)
 	{
 		free(new);
-		return (NULL)
+		return (NULL);
 	}
 	for (len = 0; str[len];)
 		len++;
@@ -32,8 +32,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	if (*head == NULL)
 	*head = new;
-
-	else 
+	else
 	{
 		last = *head;
 		while (last->next != NULL)
